2f6955804828f17782f537a93237dbbc4f1c8cdb
harshitkathuria <harshitkathuria14@gmail.com>
2021-02-26 11:48:40 +0530

    Backend Completed

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,24 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // Get all user's contact
-router.get('/', (req, res) => {
-  res.send('Get all contact');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 })
 
 // Add contact
-router.post('/', (req, res) => {
-  res.send('Add Contact');
+router.post('/', [auth, 
+  check('name', 'Name is required').not().isEmpty()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    })
+  }
+
+  const { name, email, phone, type } = req.body;
+  try {
+    const newContact = new Contact({
+      name,
+      email,
+      phone,
+      type,
+      user: req.user.id
+    });
+    
+    const contact = await newContact.save();
+    res.status(200).json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500),send('Server Error');
+  }
 })
 
 // Update contact
-router.patch('/:id', (req, res) => {
-  res.send('Update Contact');
+router.patch('/:id', auth, async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.user.id, req.body);
+  res.status(201).json({ msg: 'User Updated', user });
 })
 
 // Delete contact
-router.delete('/:id', (req, res) => {
-  res.send('Delete Contact');
+router.delete('/:id', auth, async (req, res) => {
+  const user = await User.findByIdAndDelete(req.user.id);
+  res.status(204).json({ msg: 'User Deleted '});
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect DB
 connectDB();
 
+// FOR INCOMING REQUEST BODY
+app.use(express.json({ extended: false  }));
+
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
